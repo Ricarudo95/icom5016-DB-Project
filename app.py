af506698a1f45e80953108484dc5e3076c513663
Ricarudo <ricardo.rodriguez13@upr.edu>
2018-01-09 20:57:31 -0400

    Stupid mistake ricky did

--- app.py
+++ app.py
@@ -21,47 +21,47 @@ def showResources():
 #Route used to get a resouce via its id
 @app.route('/show/resource/id/<int:rid>')
 def getResourceById(rid):
-	return ResourceHandler.getResourceById(rid)
+	return ResourceHandler().getResourceById(rid)
 
 #Route check if specific id is available.
 @app.route('/show/resource/id/<int:rid>/available')
 def checkAvailable(rid):
-	return ResourceHandler.isAvailable(rid)
+	return ResourceHandler().isAvailable(rid)
 
 #Route used to get list of supplier of specific resource.
 @app.route('/show/resource/id/<int:rid>/supplier')
 def getResourceSupplierById(rid):
-	return ResourceHandler.getResourceSupplier(rid)
+	return ResourceHandler().getResourceSupplier(rid)
 
 #Routes used to get suppliers of items in a specific location
 @app.route('/show/resource/id/<int:rid>/locate/<location>')
 def getResourcesSuppplierFrom(rid,location):
-	return ResourceHandler.fromLocation(rid,location)
+	return ResourceHandler().fromLocation(rid,location)
 
 #--------Available
 
 #Route used to get all available resouces
 @app.route('/show/resource/available')
 def getAvailableResource():
-	return ResourceHandler.getAvailableResources()
+	return ResourceHandler().getAvailableResources()
 
 
 #Route used to search available resources via keyword
 @app.route('/show/resource/available/search/<keyword>')
 def searchAvailable(keyword):
-	return ResourceHandler.searchAvailable(keyword)
+	return ResourceHandler().searchAvailable(keyword)
 
 #--------Requested
 
 #Route used to get all requested resouces
 @app.route('/show/resource/requested')
 def getRequestedResource():
-	return ResourceHandler.getRequestedResources()
+	return ResourceHandler().getRequestedResources()
 
 #Route used to search available resources via keyword. Sorted by Resouce name
 @app.route('/show/resource/requested/search/<keyword>')
 def searchRequested(keyword):
-	return ResourceHandler.searchRequested(keyword)
+	return ResourceHandler().searchRequested(keyword)
 
 #-----------------------------------------------------------------------
 
@@ -70,14 +70,14 @@ def searchRequested(keyword):
 #Basic Route that returns list of total Suppliers.
 @app.route('/show/supplier')
 def showSupplier():
-    return SupplierHandler.getAllSupplier();
+    return SupplierHandler().getAllSupplier();
 
 #--------ID
 
 #Basic Route that returns specific Suppliers.
 @app.route('/show/supplier/id/<int:sid>')
 def getSupplier(sid):
-    return SupplierHandler.getSupplierbyId(sid);
+    return SupplierHandler().getSupplierbyId(sid);
 
 #Basic Route that returns then list of resources specific supplier supplies.
 @app.route('/show/supplier/id/<int:sid>/resource')
@@ -87,7 +87,7 @@ def getSupplierResources(sid):
 #Basic Route that returns then list of resources specific supplier has sold.
 @app.route('/show/supplier/id/<int:sid>/sold')
 def getSupplierReceipts(sid):
-    return SupplierHandler.getSupplierResorceSold(sid);
+    return SupplierHandler().getSupplierResorceSold(sid);
 
 #-----------------------------------------------------------------------
 
@@ -96,19 +96,19 @@ def getSupplierReceipts(sid):
 #Basic Route that returns list of total Users.
 @app.route('/show/user')
 def showUsers():
-    return UserHandler.getAllUser();
+    return UserHandler().getAllUser();
 
 #--------ID
 
 #Basic Route that returns specific User.
 @app.route('/show/user/id/<int:uid>')
 def getUser(uid):
-    return UserHandler.getUserbyId(uid);
+    return UserHandler().getUserbyId(uid);
 
 #Basic Route that returns the list of resources specific user has bought or acquired.
 @app.route('/show/user/id/<int:uid>/acquired')
 def getUserResources(uid):
-    return UserHandler.getUserResources(uid);
+    return UserHandler().getUserResources(uid);
 
 #-----------------------------------------------------------------------
 
