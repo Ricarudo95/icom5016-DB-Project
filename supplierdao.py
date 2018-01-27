ef871d92a3eb0bfe13d11dce231c656e504cc1fa
Ricarudo <ricardo.rodriguez13@upr.edu>
2018-01-26 20:01:21 -0400

    add supplier implementation

--- supplierdao.py
+++ supplierdao.py
@@ -41,3 +41,15 @@ class SupplierDAO:
         for row in cursor:
             result.append(row)
         return result
+
+    def addSupplier(self, s_name, s_pass, s_loc):
+        cursor = self.conn.cursor()
+        address_query = "insert into supplieraddress (location_name, region, city, zip_code) values (null,null,null,null) returning sa_id"
+        cursor.execute(address_query, )
+        ua_id = cursor.fetchone()[0]
+        query = "insert into supplier(sname, pass, loc, sa_id) values ( %s, %s, %s, %s, %s ) returning s_id"
+        cursor.execute(query, (s_name,s_pass,s_loc,sa_id))
+        s_id = cursor.fetchone()[0]
+        
+        self.conn.commit()
+        return s_id
